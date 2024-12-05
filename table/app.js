fa30f7057d1918c0efc1f63a3fc0f274360f2a10
asmi902 <59359948+asmi902@users.noreply.github.com>
2024-12-05 15:46:49 +0530

    Add files via upload

--- table/app.js
+++ table/app.js
@@ -1,1 +1,9 @@
+var app = angular.module('studentApp', []);
 
+app.controller('StudentController', function($scope) {
+    $scope.students = [
+        { rollNo: 111, name: "Ram", course: "Computer" },
+        { rollNo: 112, name: "Meenu", course: "IT" },
+        { rollNo: 113, name: "Renu", course: "Mech" }
+    ];
+});
