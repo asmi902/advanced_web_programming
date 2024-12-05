5df93c14712e0dd66cb33a13172bde5ccf0c8710
asmi902 <59359948+asmi902@users.noreply.github.com>
2024-12-05 15:47:34 +0530

    Add files via upload

--- validation_form/app.js
+++ validation_form/app.js
@@ -1,1 +1,13 @@
+var app = angular.module('registrationApp', []);
 
+app.controller('FormController', function ($scope) {
+    $scope.user = {};
+
+    $scope.submitForm = function () {
+        if ($scope.registrationForm.$valid) {
+            alert('Form submitted successfully!');
+        } else {
+            alert('Form has errors. Please fix them before submitting.');
+        }
+    };
+});
